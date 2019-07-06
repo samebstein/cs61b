a8818df2ce4038ef1cc9ab182ec6a16d31d8fa05
Sam <sam.ebstein@gmail.com>
2019-07-06 12:43:38 -0700

    finished hw1

--- hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
+++ hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
@@ -1,11 +1,10 @@
 package es.datastructur.synthesizer;
+
+import java.lang.reflect.Array;
 import java.util.Iterator;
 
-//TODO: Make sure to that this class and all of its methods are public
-//TODO: Make sure to add the override tag for all overridden methods
-//TODO: Make sure to make this class implement BoundedQueue<T>
 
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T> implements BoundedQueue<T>  {
     /* Index for the next dequeue or peek. */
     private int first;
     /* Index for the next enqueue. */
@@ -14,46 +13,129 @@ public class ArrayRingBuffer<T>  {
     private int fillCount;
     /* Array for storing the buffer data. */
     private T[] rb;
+    /* Value for capacity. - Sam */
+    private int capacity;
+
+    /** Implements BoundedQueue capacity method. */
+    @Override
+    public int capacity() {
+        return capacity;
+    }
+
+    /** Implements BoundedQueue fillCount method */
+    @Override
+    public int fillCount() {
+        return fillCount;
+    }
 
     /**
      * Create a new ArrayRingBuffer with the given capacity.
      */
     public ArrayRingBuffer(int capacity) {
-        // TODO: Create new array with capacity elements.
-        //       first, last, and fillCount should all be set to 0.
+        rb = (T[]) new Object[capacity];
+        first = 0;
+        last = 0;
+        fillCount = 0;
+        this.capacity = capacity;
     }
 
     /**
      * Adds x to the end of the ring buffer. If there is no room, then
      * throw new RuntimeException("Ring buffer overflow").
      */
+    @Override
     public void enqueue(T x) {
-        // TODO: Enqueue the item. Don't forget to increase fillCount and update
-        //       last.
-        return;
+        if (fillCount == capacity) {
+            throw new RuntimeException("Ring buff overflow");
+        }
+        rb[last] = x;
+        fillCount += 1;
+
+        if (last + 1 == capacity) {
+            last = 0;
+        } else {
+            last += 1;
+        }
     }
 
     /**
      * Dequeue oldest item in the ring buffer. If the buffer is empty, then
      * throw new RuntimeException("Ring buffer underflow").
      */
+    @Override
     public T dequeue() {
-        // TODO: Dequeue the first item. Don't forget to decrease fillCount and
-        //       update first.
-        return null;
+        T returnItem = rb[first];
+        rb[first] = null;
+        fillCount -= 1;
+
+        if (first + 1 == capacity) {
+            first = 0;
+        } else {
+            first += 1;
+        }
+
+        return returnItem;
     }
 
     /**
      * Return oldest item, but don't remove it. If the buffer is empty, then
      * throw new RuntimeException("Ring buffer underflow").
      */
+    @Override
     public T peek() {
-        // TODO: Return the first item. None of your instance variables should
-        //       change.
-        return null;
+        if (fillCount == 0) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        return rb[first];
     }
 
-    // TODO: When you get to part 4, implement the needed code to support
-    //       iteration and equals.
+    private class ArrayRingBufferIterator implements Iterator<T> {
+        private int wizPos;
+
+        public ArrayRingBufferIterator() {
+            wizPos = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return wizPos < fillCount();
+        }
+
+        @Override
+        public T next() {
+            T returnItem = rb[wizPos];
+            wizPos += 1;
+            return returnItem;
+        }
+    }
+
+    public Iterator<T> iterator() {
+        return new ArrayRingBufferIterator();
+    }
+
+    public boolean equals(ArrayRingBuffer<T> other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null) {
+            return false;
+        }
+        if (other.getClass() != this.getClass()) {
+            return false;
+        }
+        ArrayRingBuffer<T> o = (ArrayRingBuffer<T>) other;
+        if (o.fillCount() != this.fillCount()) {
+            return false;
+        }
+        if (o.capacity() != this.capacity()) {
+            return false;
+        }
+        for (int i = 0; i < fillCount(); i++) {
+            if (o.rb[i] != this.rb[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
-    // TODO: Remove all comments that say TODO when you're done.
--- hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
+++ hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
@@ -1,6 +1,8 @@
 package es.datastructur.synthesizer;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.lang.reflect.Array;
+import java.util.Iterator;
 
 /** Tests the ArrayRingBuffer class.
  *  @author Josh Hug
@@ -11,4 +13,65 @@ public class TestArrayRingBuffer {
     public void someTest() {
         //ArrayRingBuffer arb = new ArrayRingBuffer(10);
     }
+
+    @Test
+    public void testArrayRB() {
+        ArrayRingBuffer<String> arb = new ArrayRingBuffer<>(4);
+        assertEquals(4, arb.capacity());
+        assertEquals(0, arb.fillCount());
+
+        arb.enqueue("one");
+        arb.enqueue("two");
+        arb.enqueue("three");
+        arb.enqueue("four");
+        assertEquals(4, arb.capacity());
+        assertEquals(4, arb.fillCount());
+
+        assertEquals("one", arb.dequeue());
+        assertEquals("two", arb.dequeue());
+        assertEquals("three", arb.dequeue());
+        assertEquals("four", arb.peek());
+        assertEquals("four", arb.dequeue());
+    }
+
+    @Test
+    public void testARBIterator() {
+        ArrayRingBuffer<String> arb = new ArrayRingBuffer<>(4);
+        arb.enqueue("one");
+        arb.enqueue("two");
+        arb.enqueue("three");
+        arb.enqueue("four");
+
+        Iterator<String> arbi = arb.iterator();
+        assertTrue(arbi.hasNext());
+        assertEquals("one", arbi.next());
+        assertTrue(arbi.hasNext());
+        assertEquals("two", arbi.next());
+        assertTrue(arbi.hasNext());
+        assertEquals("three", arbi.next());
+        assertTrue(arbi.hasNext());
+        assertEquals("four", arbi.next());
+        assertFalse(arbi.hasNext());
+    }
+
+    @Test
+    public void testEquals() {
+        ArrayRingBuffer<String> arb1 = new ArrayRingBuffer<>(3);
+        assertFalse(arb1.equals(null)); //null check
+        assertFalse(arb1.equals("hi")); //class check
+
+        ArrayRingBuffer<String> arb2 = new ArrayRingBuffer<>(3);
+        ArrayRingBuffer<String> arb3 = new ArrayRingBuffer<>(2);
+        assertTrue(arb1.equals(arb2)); //two empty queues equal
+        assertFalse(arb1.equals(arb3)); //different capacities check
+
+        ArrayRingBuffer<String> orig1 = arb1;
+        ArrayRingBuffer<String> orig2 = arb2;
+
+        arb1.enqueue("one");
+        arb2.enqueue("two");
+        assertFalse(arb1.equals(arb2)); //general not equals check
+
+    }
+
 }
