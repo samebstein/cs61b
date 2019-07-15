e53ee56b0799c250c374b7500d6d68cba3e2f997
Sam <sam.ebstein@gmail.com>
2019-07-14 21:57:05 -0700

    Percolation and TestPercolation files successful but confusing

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,176 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    Node[][] grid;
+    int openSites;
+    int N;
+    WeightedQuickUnionUF wuf;
 
+    private class Node {
+        private boolean b;
+        private int coordinate;
+        private boolean full;
+
+        public Node(int row, int col) {
+            this.coordinate = xyToCoordinate(row, col);
+            this.b = false;
+            if (coordinate < N) {
+                this.full = true;
+            } else {
+                this.full = false;
+            }
+        }
+    }
+    /** Returns an integer value given integer coordinates in a two dimensional array. */
+    public int xyToCoordinate(int row, int col) {
+        int value = row * N + col;
+        return value;
+    }
+
+    public int valueToRow(int number) {
+        return number / N;
+    }
+
+    public int valueToCol(int number) {
+        return number % N;
+    }
+
+    /** Create N-by-N grid, with all sites initially blocked. */
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        this.N = N;
+        this.openSites = 0;
+        this.wuf = new WeightedQuickUnionUF(N*N + 2);
+        grid = new Node[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                grid[i][j] = new Node(i, j);
+            }
+        }
+    }
+
+    /** open the site (row, col) if it is not open already.
+     * And also connect via union find with any other adjacent nodes. */
+    public void open(int row, int col) {
+        if (row >= N || col >= N) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        if (grid[row][col].b != true) {
+            grid[row][col].b = true;
+            openSites += 1;
+        }
+        int value = xyToCoordinate(row, col);
+        if (value < N) {
+            wuf.union(value, N*N);
+        }
+        if (value < N*N && value >= N*N - N) {
+            wuf.union(value, N*N + 1);
+        }
+        if (legitimatePlusMinusN(value + N)) {
+            int r = valueToRow(value + N);
+            int c = valueToCol(value + N);
+            if (grid[r][c].b) {
+                wuf.union(value, value + N);
+            }
+        }
+        if (legitimatePlusMinusN(value - N)) {
+            int r = valueToRow(value - N);
+            int c = valueToCol(value - N);
+            if (grid[r][c].b) {
+                wuf.union(value, value - N);
+            }
+        }
+        if (legitimatePlus1(value + 1)) {
+            int r = valueToRow(value + 1);
+            int c = valueToCol(value + 1);
+            if (grid[r][c].b) {
+                wuf.union(value, value + 1);
+            }
+
+        }
+        if (legitimateMinus1(value - 1)) {
+            int r = valueToRow(value - 1);
+            int c = valueToCol(value - 1);
+            if (grid[r][c].b) {
+                wuf.union(value, value - 1);
+            }
+        }
+    }
+    /** I want to define some function that takes the four possible adjacent coordinates and
+     * returns a boolean if they are legitimate adjacents. So it should take the coordinate of the node
+     * that we are looking at and another coordinate of the adjacent node. I will figure out the
+     * plus/minus N and the plus/minus 1 with if statements and then find statements in the open
+     * function.
+     */
+    public boolean legitimatePlusMinusN(int adjacent) {
+        return adjacent < N*N && adjacent >= 0;
+    }
+
+    public boolean legitimatePlus1(int adjacent) {
+        return adjacent % N != 0;
+    }
+
+    public boolean legitimateMinus1(int adjacent) {
+        if (adjacent < 0) {
+            return false;
+        }
+        return adjacent % N != N - 1;
+    }
+
+    /** I also want another function that determines if the adjacent is full, and if so makes the node
+     * we're looking at become full as well.
+     * This function not necessary.
+     */
+    public boolean adjacentFull(int adjacent) {
+        int row = valueToRow(adjacent);
+        int col = valueToCol(adjacent);
+
+        return grid[row][col].full;
+    }
+
+    /** and then I also want something else that changes every connected node to full if that
+     * node is now full.
+     */
+
+    /** return true if the site (row, col) is open */
+    public boolean isOpen(int row, int col) {
+        if (row >= N || col >= N) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        return grid[row][col].b;
+    }
+
+    /** return true if the site (row, col) is full */
+    // isFull is complicated. I need to figure out how to tell if this node is connected
+    // to one of the top rows' nodes and you can only connect if you connect through open nodes
+    // and to an open node on the top row.
+    public boolean isFull(int row, int col) {
+        if (row >= N || col >= N) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+        int value = xyToCoordinate(row, col);
+        if (wuf.find(value) == wuf.find(N * N)) {
+            return true;
+        }
+        return false;
+    }
+
+    /** return int number of open sites */
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    /** does the system percolate? */
+    public boolean percolates() {
+        if(wuf.find(N*N + 1) == wuf.find(N*N)) {
+            return true;
+        }
+        return false;
+    }
+
+    /** use for unit testing (not required, but keep this here for the autograder) */
+    public static void main(String[] args) {
+    }
 }
--- hw2/hw2/TestPercolation.java
+++ hw2/hw2/TestPercolation.java
@@ -7,6 +7,61 @@ public class TestPercolation {
 
     @Test
     public void testPerc() {
-        Percolation p = new Percolation(5);
+        Percolation p = new Percolation(4);
+        assertFalse(p.legitimatePlus1(0));
+        assertFalse(p.legitimatePlus1(4));
+        assertFalse(p.legitimatePlus1(8));
+        assertFalse(p.legitimatePlus1(12));
+        assertTrue(p.legitimatePlus1(3));
+        assertTrue(p.legitimatePlus1(7));
+        assertTrue(p.legitimatePlus1(15));
+        assertFalse(p.legitimateMinus1(11));
+        assertFalse(p.legitimateMinus1(3));
+        System.out.println(8 / 4);
+        System.out.println(11 / 4);
+        System.out.println(7 / 4);
+
+    }
+
+    @Test
+    public void testPerc2() {
+        Percolation p5 = new Percolation(5);
+        assertFalse(p5.isFull(4, 4));
+        assertFalse(p5.isFull(0, 0));
+        assertFalse(p5.isOpen(4, 4));
+        assertFalse(p5.isOpen(0, 0));
+        assertFalse(p5.percolates());
+        p5.open(4, 4);
+        p5.open(3, 4);
+        p5.open(3, 3);
+        p5.open(3, 2);
+        p5.open(2, 2);
+        p5.open(1, 2);
+        p5.open(1, 1);
+        assertFalse(p5.isFull(4, 4));
+        p5.open(0, 1);
+        assertTrue(p5.wuf.connected(24, 19));
+        assertTrue(p5.isFull(4, 4));
+        assertTrue(p5.percolates());
+        assertTrue(p5.isFull(4, 4));
+        assertTrue(p5.isFull(3,4));
+        assertTrue(p5.isFull(3, 3));
+        p5.open(4, 2);
+        assertTrue(p5.isFull(4, 2));
+    }
+
+    @Test
+    public void testPerc3() {
+        Percolation p3 = new Percolation(3);
+        assertFalse(p3.isFull(0, 0));
+        assertFalse(p3.percolates());
+        p3.open(0,2);
+        assertFalse(p3.isFull(0, 0));
+        p3.open(1,0);
+        p3.open(1,1);
+        p3.open(1,2);
+        p3.open(2,0);
+        assertTrue(p3.isFull(2, 0));
+        assertTrue(p3.percolates());
     }
 }
