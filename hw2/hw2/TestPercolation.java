8cee1228c0363c3fd954bb5062a3b44f6b3c94d6
Sam <sam.ebstein@gmail.com>
2019-07-14 21:56:05 -0700

    Test and scratchwork files

---
+++ hw2/hw2/TestPercolation.java
@@ -0,0 +1,12 @@
+package hw2;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestPercolation {
+
+    @Test
+    public void testPerc() {
+        Percolation p = new Percolation(5);
+    }
+}
